185a1b2e8969f3e91afb3b3cf3aa73c0d60bb5bc
Bence Kiglics <bence.kiglics@gmail.com>
2019-02-09 10:37:08 +0100

    Only raise when we have a pair

--- player.py
+++ player.py
@@ -3,7 +3,14 @@ class Player:
     VERSION = "Default Python 3 folding player"
 
     def betRequest(self, game_state):
-        return 1000
+        current_buy_in = game_state['current_buy_in']
+        player = game_state['players'][game_state['in_action']]
+        minimum_raise = game_state['minimum_raise']
+
+        if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank']:
+            return current_buy_in - player['bet'] + minimum_raise
+
+        return 0
 
     def showdown(self, game_state):
         pass
