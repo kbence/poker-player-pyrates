84af5622b8b5d9eeaed626c0abf0adfc7d1bae66
Lete <nnlete@gmail.com>
2019-02-09 10:56:47 +0100

    Simple high card

--- player.py
+++ player.py
@@ -7,11 +7,31 @@ class Player:
         player = game_state['players'][game_state['in_action']]
         minimum_raise = game_state['minimum_raise']
 
-        if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank']:
+        c1_card = player['hole_cards'][0]
+        c2_card = player['hole_cards'][1]
+
+        card_val_map = {
+            'A' : 10,
+            'K' : 8,
+            'Q' : 7,
+            'J' : 6
+        }
+        ranks = ['A', 'K', 'Q', 'J'] + list(map(str, range(10, 1, -1)))
+
+        c1_rank = ranks.index(c1_card['rank'])
+        c2_rank = ranks.index(c2_card['rank'])
+
+        if c1_rank + c2_rank >= 18:
             return current_buy_in - player['bet'] + minimum_raise
+
+        if player['hole_cards'][0]['rank'] == player['hole_cards'][1]['rank']:
+            return current_buy_in - player['bet'] + minimum_raise * 2
 
         return 0
 
     def showdown(self, game_state):
         pass
 
+    @staticmethod
+    def is_pair(c1, c2):
+        return c1['rank'] == c2['rank']
